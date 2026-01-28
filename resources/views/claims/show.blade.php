cc5c952a2c7e532feec07481617491693b2b899a
Amir mustofa <amirmustofa814@gmail.com>
2026-01-28 14:33:10 +0700

    pembaruan cek kecocokan untuk halaman admin

--- resources/views/claims/show.blade.php
+++ resources/views/claims/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Detail Klaim Barang') }} 🔍
+            {{ __('Detail Klaim Barang') }} 
         </h2>
     </x-slot>
 
